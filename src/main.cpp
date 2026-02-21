c80de21f99c666f9ad8026baebd2d2dd88f491c9
Tanner Davison <tanner.davison95@gmail.com>
2026-02-20 22:38:53 -0700

    added copyright and added .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
 // SDL3 Starter Template
-
+// Copyright (c) 2026 Tanner Davison
 #include "window.hpp"
 #include <SDL3/SDL_main.h>
 
-int main(int argc, char *argv[]) {
-	 try {
-			SDLWindow gameWindow(800, 600, "My SDL Window");
+int main(int argc, char* argv[]) {
+   try {
+      SDLWindow gameWindow(800, 600, "My SDL Window");
 
-			// Main game loop
-			while (!gameWindow.shouldClose()) {
-				 gameWindow.handleEvents();
+      // Main game loop
+      while (!gameWindow.shouldClose()) {
+         gameWindow.handleEvents();
 
-				 // Clear screen
-				 SDL_SetRenderDrawColor(gameWindow.getRenderer(), 0, 0, 0, 255);
-				 SDL_RenderClear(gameWindow.getRenderer());
+         // Clear screen
+         SDL_SetRenderDrawColor(gameWindow.getRenderer(), 0, 0, 0, 255);
+         SDL_RenderClear(gameWindow.getRenderer());
 
-				 // TODO: Render game objects
+         // TODO: Render game objects
 
-				 SDL_RenderPresent(gameWindow.getRenderer());
-			}
-	 } catch (const std::exception &e) {
-			printf("Error: %s\n", e.what());
-			return -1;
-	 }
+         SDL_RenderPresent(gameWindow.getRenderer());
+      }
+   } catch (const std::exception& e) {
+      printf("Error: %s\n", e.what());
+      return -1;
+   }
 
-	 return 0;
+   return 0;
 }
