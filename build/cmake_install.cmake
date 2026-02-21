73232588172a8cac5b83949f78edd287fe10abb6
Tanner Davison <tanner.davison95@gmail.com>
2026-02-20 21:47:26 -0700

    fixed CMakeLists.txt

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tanner/projects/cpp/sdl-starter
+# Install script for directory: /home/tanner/projects/cpp/sdl2-starter-template
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tanner/projects/cpp/sdl-starter/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tanner/projects/cpp/sdl2-starter-template/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
